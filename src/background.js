9b2689fdb07f7e5364e0a496a38146500a19ec55
nirLumigo <nir@lumigo.io>
2022-06-08 16:14:15 +0300

    url to history object

--- src/background.js
+++ src/background.js
@@ -26,24 +26,44 @@ async function addHistoryObject(obj) {
   await saveHistoryObjects(historyObjects);
 }
 
+function urlToHistoryObject(url) {
+    const region = url.split("//")[1].split(".")[0]
+    const commonObj = {counter: 1, region}
+    if (!url.includes("console.aws.amazon.com")) {
+        return null
+    }
+    if (url.includes("console.aws.amazon.com/lambda/") && url.includes("/functions/")) {
+        let name = url.split("/")[url.split("/").length - 1];
+        if (name.includes("?")) {
+            const cleanName = name.substr(0, name.indexOf("?"));
+            url = url.replace(name, cleanName);
+            name = cleanName;
+        }
+        return {name, url, service: "lambda-function", ...commonObj}
+    }
+}
 
 async function addUrl(url) {
     let historyObjects = await getHistoryObjects();
-    let existingObj = historyObjects.find(obj => obj.url === url);
+    const historyObj = urlToHistoryObject(url)
+    let existingObj = historyObjects.find(obj => obj.url === historyObj.url);
     if (existingObj) {
         existingObj.counter++;
-    } else {
-        historyObjects.push({name: "temp-name", url, service: "temp-service", counter: 0})
+        await saveHistoryObjects(historyObjects);
+        return;
     }
-    await saveHistoryObjects(historyObjects);
+    if (historyObj) {
+        historyObjects.push(historyObj);
+        await saveHistoryObjects(historyObjects);
+    }
 }
 
-chrome.tabs.onUpdated.addListener(function
+chrome.tabs.onUpdated.addListener(async function
   (tabId, changeInfo, tab) {
     // read changeInfo data and do something with it (like read the url)
     if (changeInfo.url) {
       // alert("URL was changed: " + changeInfo.url + "FROM background");
-        addUrl(changeInfo.url);
+        await addUrl(changeInfo.url);
     }
   }
 );
