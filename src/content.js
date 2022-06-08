a49127e25eb8020c300197afff7e7f262bdd92bd
CircleCI <no-reply@build.com>
2022-06-08 15:29:04 +0300

    html dialog

--- src/content.js
+++ src/content.js
@@ -6,10 +6,37 @@ window.addEventListener("popup-modal", function(evt) {
   // We need to have this function in order for the listeners to be received
 }, false);
 
+const showBuddyModal = () => {
+    const modal = document.createElement("dialog");
+    modal.setAttribute(
+        "style", `
+height:300px;
+width: 50%;
+border: none;
+top:150px;
+border-radius:20px;
+background-color:white;
+position: fixed; box-shadow: 0px 12px 48px rgba(29, 5, 64, 0.32);
+`
+    );
+    modal.innerHTML =  `<div>
+This is our content
+</div>`;
+    document.body.appendChild(modal);
+    const dialog = document.querySelector("dialog");
+    dialog.showModal();
+    const iframe = document.getElementById("popup-content");
+    // iframe.src = chrome.extension.getURL("index.html");
+    iframe.frameBorder = 0;
+    dialog.querySelector("button").addEventListener("click", () => {
+        dialog.close();
+    });
+}
+
 
 (chrome || browser).runtime.onMessage.addListener(function(msg, sender, cb) {
   const { data, action } = msg;
   if (action === 'openBuddy') {
-    document.getElementsByClassName("awsui_heading-text_2qdw9_s2c65_262")[0].childNodes[0].data = 'Bla';
+        showBuddyModal();
   }
 });
